e25ab7f76f2ab5965181bfdd213e78aa972adc6f
Noel Vock <noel1uk@gmail.com>
2018-05-02 17:11:20 +0100

    feat: YearButtonContainer renders a YearButton

--- src/components/YearButtonContainer.js
+++ src/components/YearButtonContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import YearButton from './YearButton';
 
 class YearButtonContainer extends Component {
-
+  render() {
+    return (
+      <div>
+        <YearButton />
+      </div>
+    )
+  }
 }
 
 export default YearButtonContainer;
