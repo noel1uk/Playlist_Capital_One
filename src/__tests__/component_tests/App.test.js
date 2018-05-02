15b3fb0c0c6db19f608b385409e92ab3c999703d
Noel Vock <noel1uk@gmail.com>
2018-05-02 15:05:25 +0100

    test: ensure app contains a YearButtonContainer

--- src/__tests__/component_tests/App.test.js
+++ src/__tests__/component_tests/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../components/App';
+import YearButtonContainer from '../../components/YearButtonContainer';
 
 const app = shallow(<App />);
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
 });
+
+it('renders a YearButtonContainer component', () => {
+  expect(app.find('YearButtonContainer').exists()).toBe(true);
+} )
