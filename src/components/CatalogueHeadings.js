b42ebe9038b81d6065ae3fe1ca80f91110d70510
Noel Vock <noel1uk@gmail.com>
2018-05-02 18:33:10 +0100

    feat: add table row with relevant headings

---
+++ src/components/CatalogueHeadings.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CatalogueHeadings extends Component {
+  render() {
+    return (
+      <tr>
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Album</th>
+        <th>Year</th>
+      </tr>
+    )
+  }
+}
+
+export default CatalogueHeadings;
