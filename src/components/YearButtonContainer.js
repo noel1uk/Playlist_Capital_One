64bfb55fffff947dc4fc1f762e62932b66ca5649
Noel Vock <noel1uk@gmail.com>
2018-05-02 16:07:37 +0100

    fix: remove superflous elements from YearButtonContainer

--- src/components/YearButtonContainer.js
+++ src/components/YearButtonContainer.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
 class YearButtonContainer extends Component {
-  render() {
-    return (
-      <div></div>
-    )
-  }
+
 }
 
 export default YearButtonContainer;
