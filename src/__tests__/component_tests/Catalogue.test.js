44ae96a4f5a86fda13ee4252feb6f3fe4fd6ca6a
Noel Vock <noel1uk@gmail.com>
2018-05-02 18:24:24 +0100

    test: ensure Catalogue renders catalogue headings

--- src/__tests__/component_tests/Catalogue.test.js
+++ src/__tests__/component_tests/Catalogue.test.js
@@ -6,3 +6,7 @@ const catalogue = shallow(<Catalogue />);
 it('renders correctly', () => {
   expect(catalogue).toMatchSnapshot();
 });
+
+it('renders catalogue headings', () => {
+  expect(catalogue.find('CatalogueHeadings').exists()).toBe(true)
+});
