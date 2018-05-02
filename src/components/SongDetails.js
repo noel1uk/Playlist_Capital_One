e8873ae27e8b420e6f364d91a744ca826169e317
Noel Vock <noel1uk@gmail.com>
2018-05-02 19:14:14 +0100

    feat: add filler text

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class SongDetails extends Component {
-
+  render() {
+    return (
+      <tr>
+      <td>One More Time</td>
+      <td>Alison Moyet</td>
+      <td>The Turn</td>
+      <td>2007</td>
+    </tr>
+    )
+  }
 }
 
 export default SongDetails;
