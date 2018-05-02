45cad93c3736a409ca69ae7749d317fcf0c5de87
Noel Vock <noel1uk@gmail.com>
2018-05-02 19:05:46 +0100

    feat: add SongDetails component to Catalogue

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CatalogueHeadings from './CatalogueHeadings';
+import SongDetails from './SongDetails';
 
 class Catalogue extends Component {
   render() {
     return (
       <table>
         <CatalogueHeadings />
+        <SongDetails />
       </table>
     )
   }
