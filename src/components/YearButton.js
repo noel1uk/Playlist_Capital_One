8f1e2291cdfecd99bea5288faea9cd5d71dfa85a
Noel Vock <noel1uk@gmail.com>
2018-05-02 17:31:06 +0100

    feat: year button renders

--- src/components/YearButton.js
+++ src/components/YearButton.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class YearButton extends Component {
-
+  render() {
+    return (
+      <button>2007</button>
+    )
+  }
 }
 
 export default YearButton;
