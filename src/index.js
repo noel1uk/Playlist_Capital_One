9172a4932342f1cb3e2d6199248cbd5c91070fa2
Noel Vock <noel1uk@gmail.com>
2018-05-03 02:45:49 +0100

    feat: add keys to dataset

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 const MUSIC = [
-  {year: '2007', title: 'One More Time', artist: 'Alison Moyet', album: 'The Turn'},
-  {year: '2007', title: 'Another Day', artist: 'Alison Moyet', album: 'The Turn'},
-  {year: '2010', title: 'Love Dance', artist: 'The Cure', album: 'Disintegration'},
-  {year: '2013', title: 'Taurus II', artist: 'Mike Oldfield', album: '5 Miles Out'},
-  {year: '2014', title: 'The Children Of Yarn', artist: 'T-Rex', album: 'T-Rex'},
-  {year: '2015', title: 'F4', artist: 'Spooky Tooth', album: 'The Island Years'},
-  {year: '2015', title: 'Face-off', artist: 'Spooky Tooth', album: 'The Island Years'}
+  {key: 1, year: '2007', title: 'One More Time', artist: 'Alison Moyet', album: 'The Turn'},
+  {key: 2, year: '2007', title: 'Another Day', artist: 'Alison Moyet', album: 'The Turn'},
+  {key: 3, year: '2010', title: 'Love Dance', artist: 'The Cure', album: 'Disintegration'},
+  {key: 4, year: '2013', title: 'Taurus II', artist: 'Mike Oldfield', album: '5 Miles Out'},
+  {key: 5, year: '2014', title: 'The Children Of Yarn', artist: 'T-Rex', album: 'T-Rex'},
+  {key: 6, year: '2015', title: 'F4', artist: 'Spooky Tooth', album: 'The Island Years'},
+  {key: 7, year: '2015', title: 'Face-off', artist: 'Spooky Tooth', album: 'The Island Years'}
 ];
 
 render(
