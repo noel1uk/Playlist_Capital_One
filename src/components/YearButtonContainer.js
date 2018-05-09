6f2156f0973d93cc543f7d2c71fd322caf2dfc16
Noel Vock <noel1uk@gmail.com>
2018-05-09 14:40:00 +0100

    refactor: get rid of YearButton

--- src/components/YearButtonContainer.js
+++ src/components/YearButtonContainer.js
@@ -1,39 +1,7 @@
 import React, { Component } from 'react';
-import YearButton from './YearButton';
 
 class YearButtonContainer extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     age: 100
-  //   };
-  // }
-  // buttons = props => {
-  //   let buttons = [];
-  //   const music = props;
-  //   let uniqueYears = [];
-  //   if(music !== undefined){
-  //     music.forEach((song) => {
-  //       if ( uniqueYears.includes(song.year) === false ) {
-  //         uniqueYears.push(song.year);
-  //       }
-  //     })
-  //   }
-  //   uniqueYears.forEach((year) => {
-  //     buttons.push(<YearButton 
-  //       year={year}
-  //       />);
-  //   })
-  //   // this.state = buttons;
-  //   console.log(this.state);
-  //   if ( this.state.age === 100 ) {
-  //     this.setState({buttons})
-
-  //   }
-  //   // return buttons;
-  // }
   render() {
-    console.log(this);
     return (
       <div>
         <button>2007</button>
