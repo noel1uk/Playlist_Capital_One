0df6a5715a0285982eb8187a83941fd4ce90f2e1
Noel Vock <noel1uk@gmail.com>
2018-05-10 13:48:21 +0100

    feat: generate buttons from state

--- src/components/YearButtonContainer.js
+++ src/components/YearButtonContainer.js
@@ -16,11 +16,11 @@ class YearButtonContainer extends Component {
   render() {
     return (
       <div>
-        <button>2007</button>
-        <button>2010</button>
-        <button>2013</button>
-        <button>2014</button>
-        <button>2015</button>
+        {this.state.arr.map((element, index) =>
+          <button>
+            {element.name}
+          </button>
+        )}
       </div>
     )
   }
